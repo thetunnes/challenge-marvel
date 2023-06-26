f3ecefe07a3e8750b12864bbf98d928f2424f162
Tunnes <cauepani06@gmail.com>
2023-06-26 17:39:31 -0300

    add:Adding link connect with Roboto Google Fonts

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,8 +9,8 @@ export function Home() {
       <h1>Marvel API</h1>
       <p>
         This project was developed as a challenge, where I wanted to create a
-        Front-end application that consumes data from the Rest of Marvel Studios
-        API
+        Front-end application that consumes data from the Rest API of Marvel
+        Studios.
       </p>
       <InputSearch />
     </HomeContainer>
